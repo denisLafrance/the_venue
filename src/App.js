6a353f27cd336b4540f796cc672e34a0c1e818c2
denisLafrance <48102747+denisLafrance@users.noreply.github.com>
2022-05-31 19:37:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './resources/styles.css';
+import { Element} from 'react-scroll';
+import Header from './components/header_footer/Header';
+import Featured from './components/featured/index';
+import VenueInfo from './components/venueInfo/index';
+import Highlights from './components/highlights/index';
+import Pricing from './components/pricing/index';
+import Location from './components/location/index';
+import Footer from './components/header_footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: "1550px", background:'cornflowerblue'}}>
+      <Header />
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="venueInfo">
+        <VenueInfo />
+      </Element>
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return(
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. 
+                </div>
+            </div>
+        </Fade>
+    )
+}
+
+export default Description;
